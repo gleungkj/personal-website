82912639fa2e0faea69cbb9c0d48d73764dc36c6
Gavin <gavin@primary-portal.com>
2023-12-19 19:45:34 +0000

    feat(middleware.ts): add authentication middleware for '/developer' route

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,16 @@
 import Link from "next/link"
 import styles from '../app/page.module.css'
+import Head from "next/head"
 
 export const Header = (): JSX.Element => {
     return (
+      <div>Name Here    
         <div className={styles.grid}>        
-        <Link href='/about'>About</Link>
-        <Link href='/works'>Works</Link>
-        <Link href='/contact'>Contact</Link>
-        <Link href='/developer'>Developer Portal</Link>
-      </div>
+          <Link href='/about'>About</Link>
+          <Link href='/works'>Works</Link>
+          <Link href='/contact'>Contact</Link>
+          <Link href='/developer'>Developer Portal</Link>
+        </div>
+      </div>    
     )
 }
