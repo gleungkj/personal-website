5f8d7e962aa7209320c5ea6f7e042af7e390d865
Gavin <gavin@primary-portal.com>
2024-08-22 14:18:43 +0100

    feat(svg.d.ts): add SVG support

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,17 @@
 'use client'
 
 import styles from './Footer.module.css'
+import VercelLogo from '@/app/assets/vercelLogo'
+import ReactLogo from '@/app/assets/reactLogo'
+import NextJSLogo from '@/app/assets/nextJSLogo'
 
 export const Footer = (): JSX.Element => {
-    return (<div className={styles.footer}>Footer</div>)
+    return (
+        <div className={styles.footer}>
+            <div>Powered by </div>
+            <NextJSLogo height="56px" width="216px" />
+            <VercelLogo height="56px" width="216px" />
+            <ReactLogo height="56px" width="216px" />
+        </div>
+    )
 }
