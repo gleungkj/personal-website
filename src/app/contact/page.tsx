f63e6a26f470b81b7d17d840d14d9976181d7e1b
Gavin <gavin@primary-portal.com>
2024-09-18 13:25:49 +0100

    refactor(package-lock.json): update dependencies and refactor clerk components

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,20 +1,20 @@
-import { prisma } from "@/../server";
-import { PageTable } from "@/components/PageTable";
-import { IWebsiteContents, websitePageType } from "@/constants/websiteContents";
-import { auth } from "@clerk/nextjs";
+import { prisma } from '@/../server'
+import { PageTable } from '@/components/PageTable'
+import { IWebsiteContents, websitePageType } from '@/constants/websiteContents'
+import { auth } from '@clerk/nextjs/server'
 
 export default async function ContactPage() {
+    const authenticationDetails = await auth()
 
-  const authenticationDetails = await auth()
+    const isAdmin = authenticationDetails.orgRole === 'org:admin'
 
-  const isAdmin = authenticationDetails.orgRole === 'org:admin'
-
-  const contactData: IWebsiteContents[] | null = await prisma.website.findMany({
-    where: { page: websitePageType.contact },
-  });
-  return contactData !== null ? (
-    <PageTable websiteContents={contactData} isAdmin={isAdmin}/>
-  ) : (
-    <div></div>
-  );
+    const contactData: IWebsiteContents[] | null =
+        await prisma.website.findMany({
+            where: { page: websitePageType.contact },
+        })
+    return contactData !== null ? (
+        <PageTable websiteContents={contactData} isAdmin={isAdmin} />
+    ) : (
+        <div></div>
+    )
 }
